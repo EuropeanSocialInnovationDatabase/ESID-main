2276607e76996f39e76877c1684bdda3f7dc7395
Nikola Milosevic <nikola.milosevic86@gmail.com>
2017-09-08 15:03:19 +0100

    SIMRA scraper finished

--- ESIDcrawlers/ESIDcrawlers/settings.py
+++ ESIDcrawlers/ESIDcrawlers/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'ESIDcrawlers.spiders'
 #This is needed for overcaming restriction in robots.txt
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
 CONCURRENT_REQUESTS = 32
-DOWNLOAD_DELAY = 5
+DOWNLOAD_DELAY = 0
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'ESIDcrawlers (+http://www.yourdomain.com)'
 
