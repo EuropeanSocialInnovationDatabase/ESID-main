9c24453585fbd3451048ec5989f6fb4a36961b17
nikola.milosevic86@gmail.com <nikola.milosevic86@gmail.com>
2018-01-01 20:26:51 +0000

    Trainers correctly counding TP

--- TextMining/Classifiers/Trainers/ANN_Trainer_Actors.py
+++ TextMining/Classifiers/Trainers/ANN_Trainer_Actors.py
@@ -571,11 +571,11 @@ if __name__ == '__main__':
         for pred in predictions:
             print "Prediction: "+str(pred)
             print "Y valuation: "+str(y_val[i])
-            if pred[1] > 0.5 and y_val[i][0] == 1:
+            if pred[1] > 0.5 and y_val[i][1] == 1:
                 TP_sum = TP_sum + 1
-            if pred[1] > 0.5 and y_val[i][0]==0:
+            if pred[1] > 0.5 and y_val[i][1]==0:
                 FP_sum = FP_sum + 1
-            if pred[1] < 0.5 and y_val[i][0]==1:
+            if pred[1] < 0.5 and y_val[i][1]==1:
                 FN_sum = FN_sum + 1
             i = i+1
         number_samples = len(predictions)
