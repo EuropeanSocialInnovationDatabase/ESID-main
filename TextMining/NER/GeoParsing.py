ab3e5eda1b2c149ddbe371d279a4ae433e664191
Nikola Milosevic <nikola.milosevic86@gmail.com>
2018-01-16 15:03:11 +0000

    Geoparsing and NER

--- TextMining/NER/GeoParsing.py
+++ TextMining/NER/GeoParsing.py
@@ -1,6 +1,20 @@
-from mordecai import Geoparser
-geo = Geoparser()
-naa = geo.geoparse("I traveled from Oxford to Ottawa.")
-print(naa)
-baa = geo.geoparse("My address is Pedje Milosavljevica 74, Belgrade, Serbia")
-print(baa)
+#from mordecai import Geoparser
+from geopy.geocoders import Nominatim
+import pyap
+#geo = Geoparser()
+#naa = geo.geoparse("I traveled from Oxford to Ottawa.")
+#print(naa)
+#baa = geo.geoparse("My address is Pedje Milosavljevica 74, Belgrade, Serbia")
+#print(baa)
+geolocator = Nominatim()
+location = geolocator.geocode("Pedje Milosavljevica 74, Belgrade, Serbia")
+print(location.address)
+
+test_address = """
+    Lorem ipsum 225 E. John Carpenter Freeway, Suite 1500 Irving, Texas 75062 Dorem sit amet
+    """
+addresses = pyap.parse(test_address, country='US')
+for address in addresses:
+    print(address)
+    # shows address parts
+    print(address.as_dict())
