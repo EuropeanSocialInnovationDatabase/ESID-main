4c68e926427987a8da8d5b3c12e4a459600ddb4e
Iffat Gheyas <iffat.gheyas@strath.ac.uk>
2019-11-07 14:36:45 +0000

    added again print in AddMissingWebsitePenny.py

--- Helpers/AddMissingWebsitesPenny.py
+++ Helpers/AddMissingWebsitesPenny.py
@@ -3,7 +3,7 @@ import MySQLdb
 import csv
 
 from database_access import *
-print("aaa")
+print("bbb")
 db = MySQLdb.connect(host, username, password, database, charset='utf8')
 cursor = db.cursor()
 csvfile =  open('MissingWebsitesESID_Task_1_Penny_UPDATED_edit_31.07.2018.csv', 'rb')
@@ -11,7 +11,7 @@ csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
 for row in csvreader:
     penny =  row[3]
     id = row[0]
-    print str(id) + "  "+ str(penny)
+    print(str(id) + "  "+ str(penny))
     if "http" in penny:
         sql = "Update Projects set ProjectWebpage='{0}', HadMissingWebpage=2 where idProjects={1} and HadMissingWebpage=2".format(penny,id)
         cursor.execute(sql)
